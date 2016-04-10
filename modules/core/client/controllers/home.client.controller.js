d65737100824553578299b56b3c2f09e66165398
joanclopez <joanclopezm@gmail.com>
2016-04-10 17:58:18 -0600

    modify home

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -6,6 +6,41 @@ angular.module('core').controller('HomeController', ['$scope','$state', 'Authent
      $scope.dt = new Date(2016, 4, 7);
     $scope.$state = $state;
     $scope.authentication = Authentication;
+
+    if ($scope.authentication.user) {
+      if (!$scope.authentication.user.smash4.register&&!$scope.authentication.user.smashMelee.register) {
+        var modalInstance = $modal.open({
+          animation: true,
+          templateUrl: 'modalSelectTournament.html',
+          controller: function ($scope, $modalInstance,Users,Authentication) {
+            $scope.user=Authentication.user;
+            console.log($scope.user.smash4);
+            $scope.smash4Register=$scope.user.smash4.register;
+            $scope.smashMeleeRegister=$scope.user.smashMelee.register;
+            $scope.ok = function () {
+                $scope.user.smash4.register=$scope.smash4Register;
+                $scope.user.smashMelee.register=$scope.smashMeleeRegister;
+              var user = new Users($scope.user);
+
+              user.$update(function (response) {
+                alert('Tu registro fue guardado, recuerda pagar tu inscripción');
+                $scope.success = true;
+                Authentication.user = response;
+              }, function (response) {
+                $scope.error = response.data.message;
+              });
+              $modalInstance.close();
+            };
+            $scope.cancel = function () {
+              $modalInstance.dismiss('cancel');
+            };
+          },
+        });
+
+        modalInstance.result.then(function () {
+        }, function () {
+        });      }
+    }
     $scope.resources = [
       'Smash.mp4',
 			'Smash.webm'
@@ -17,12 +52,47 @@ angular.module('core').controller('HomeController', ['$scope','$state', 'Authent
     $scope.playInfo = {};
     $scope.pausePlay = true;
 
-    $scope.open = function (size) {
+    $scope.open = function () {
       var modalInstance = $modal.open({
         animation: true,
         templateUrl: 'myModalContent.html',
         controller: function ($scope, $modalInstance) {
           $scope.ok = function () {
+            $modalInstance.close();
+          };
+          $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+          };
+        },
+      });
+
+      modalInstance.result.then(function () {
+      }, function () {
+      });
+    };
+
+    $scope.openPlatformSelect = function () {
+      console.log('entro');
+      var modalInstance = $modal.open({
+        animation: true,
+        templateUrl: 'modalSelectTournament.html',
+        controller: function ($scope, $modalInstance,Users,Authentication) {
+          $scope.user=Authentication.user;
+          console.log($scope.user.smash4);
+          $scope.smash4Register=$scope.user.smash4.register;
+          $scope.smashMeleeRegister=$scope.user.smashMelee.register;
+          $scope.ok = function () {
+              $scope.user.smash4.register=$scope.smash4Register;
+              $scope.user.smashMelee.register=$scope.smashMeleeRegister;
+            var user = new Users($scope.user);
+
+            user.$update(function (response) {
+              alert('Tu registro fue guardado, recuerda pagar tu inscripción');
+              $scope.success = true;
+              Authentication.user = response;
+            }, function (response) {
+              $scope.error = response.data.message;
+            });
             $modalInstance.close();
           };
           $scope.cancel = function () {
